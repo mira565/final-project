650b6604a1d220e3972c28587aa8fe166b2b99a8
Mira <miratulebaeva@gmail.com>
2022-09-22 17:54:42 +0500

    changes in main.scss were added

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build 
+
+cd dist
+
+git init
+git add -Ach
+git commit -m 'New Deployment'
+git push -f git@github.com:mira565/mira-project.git master:gh-pages
+
+cd-
